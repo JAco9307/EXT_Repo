18aff3281da11eb4e652bcbca74614ebf9f1fbb3
JAco9307 <skrivtiljacob@hotmail.com>
2020-12-07 14:54:45 +0100

    Basic GPIO ouput test

--- OtherFiles/MotorTestFiles/MotorTest.cpp
+++ OtherFiles/MotorTestFiles/MotorTest.cpp
@@ -3,10 +3,13 @@
 #include <sys/ipc.h>
 #include <sys/shm.h> 
 #include <stdio.h> 
-
+#include <wiringPi.h>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-    
+    wiringPiSetup();
+    pinMode(0,OUTPUT);
+    digitalWrite(0, atoi(argv[1])); //works
     return 0;
 }
