1e8b241180de0487db529c6a349845717677914a
JAco9307 <skrivtiljacob@hotmail.com>
2020-12-07 15:46:03 +0100

    Moves CLI to bin so its accessible everyhwere

--- OtherFiles/compile.sh
+++ OtherFiles/compile.sh
@@ -1,1 +1,1 @@
-g++ -o MotorController DaemonTest.cpp; g++ -o CLI ValueUpdate.cpp; echo "Done" 
+g++ -o MotorController DaemonTest.cpp; g++ -o CLI ValueUpdate.cpp; echo "Done"; sudo mv CLI /usr/bin/
