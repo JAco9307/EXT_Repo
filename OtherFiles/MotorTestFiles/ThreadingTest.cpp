43451902fa6f8296ae75c9a5a57946220aced65b
JAco9307 <skrivtiljacob@hotmail.com>
2020-12-09 10:22:41 +0100

    testing threading

---
+++ OtherFiles/MotorTestFiles/ThreadingTest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h> 
+#include <stdio.h> 
+
+using namespace std;
+#define EVER ;;
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
