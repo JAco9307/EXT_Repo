d63fb8098b7416d20a5375c1e464dc60e6b3e252
fivanovic <fivanovic1999@gmail.com>
2020-12-18 11:00:52 +0100

    try catch jank implemented

--- Joystick/JoystickInterpreter.py
+++ Joystick/JoystickInterpreter.py
@@ -17,6 +17,11 @@ sensor2 = 0
 sensor3 = 0
 sensor4 = 0
 
+sensor1prev = 0
+sensor2prev = 0
+sensor3prev = 0
+sensor4prev = 0
+
 setting = input("Stream? ")
 strmset = 'stream %s' %(setting)
 nostrmset = 'stream 0'
@@ -53,31 +58,39 @@ downvalavg = 0
 total = 0
 zout = 0
 
-def StreamReader(box1,s1,s2,V,Z,grad,dead,out,deadzone):
+yread = [0,0,0]
+xread = [0,0,0]
+
+def StreamReader(box1,s1,s2,s1prev,s2prev,V,Z,grad,dead,out,deadzone):
     resp = ser.readline()
     res = resp.decode()
     #this line takes only the integers from the stream response
     box1 = [int(s) for s in re.findall(r'\d+', res)]
-    s1 = box1[1]
-    s2 = box1[3]
+    try:
+        s1 = box1[1]
+    except:
+        s1 = s1prev
+        print('catch!')
+    try:
+        s2 = box1[3]
+    except:
+        s2 = s2prev
+        print('catch!')
     V[0] = s1-s2
     V[1] = (V[0]-Z[0])/grad
     #Dead zone and upper limit calculation
     if V[1] > (dead+deadzone) or V[1] < (dead-deadzone):
         if V[1] > 200:
             out = 200
-            print(out)
         elif V[1] < -200:
             out = -200
-            print(out)
         else:
             out = int(V[1])
-            print(out)
     else:
         out = 0
-        print(out)
-
-    return out
+    s1prev = s1
+    s2prev = s2
+    return [out,s1prev,s2prev]
 
 def UpDown(yO,xO,upval,downval,zO,s1,s2,s3,s4):
     if yO == 0 and xO == 0:
@@ -89,7 +102,7 @@ def UpDown(yO,xO,upval,downval,zO,s1,s2,s3,s4):
             zO = 0
             print('up')
     return zO
-    
+
 while(True):
     try:
         if a <=0:
@@ -113,18 +126,24 @@ while(True):
             upvalavg = vals[10]
             downvalavg = vals[11]
             a+=1
-            time.sleep(0.005)
+            time.sleep(0.05)
 
         else:
             #Y axis Processing
-            yout = StreamReader(testres,sensor1,sensor2,yvalue,yzero,ygrad,ydead,yout,deadZ)
+            yread = StreamReader(testres,sensor1,sensor2,sensor1prev,sensor2prev,yvalue,yzero,ygrad,ydead,yout,deadZ)
+            yout = yread[0]
+            sensor1prev = yread[1]
+            sensor2prev = yread[2]
             #X axis Processing
-            xout = StreamReader(testres,sensor3,sensor4,xvalue,xzero,xgrad,xdead,xout,deadZ)
+            xread = StreamReader(testres,sensor3,sensor4,sensor3prev,sensor4prev,xvalue,xzero,xgrad,xdead,xout,deadZ)
+            xout = yread[0]
+            sensor3prev = yread[1]
+            sensor4prev = yread[2]
             #updown processing
             zout=UpDown(yout,xout,upvalavg,downvalavg,zout,sensor1,sensor2,sensor3,sensor4)
             #sending to the controller
-            #os.system("CLI " + str(xout) + " " + str(yout) + " " + str(int(zout)))
-            time.sleep(0.005)
+            os.system("CLI " + str(xout) + " " + str(yout) + " " + str(int(zout)))
+            time.sleep(0.05)
 
     except KeyboardInterrupt:
         os.system("CLI")
